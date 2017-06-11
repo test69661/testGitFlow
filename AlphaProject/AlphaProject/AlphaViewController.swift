ffb44520482c10492cfa42dc4a6d25a0a932ba5a
Bob <test69661@gmail.com>
2017-06-11 23:51:05 +0200

    test

---
+++ AlphaProject/AlphaProject/AlphaViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AlphaViewController.swift
+//  AlphaProject
+//
+//  Created by Cociu Laurentiu-Victor on 11/06/2017.
+//  Copyright © 2017 SomeCompany. All rights reserved.
+//
+
+import UIKit
+
+class AlphaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+sdfsdfsdß
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
