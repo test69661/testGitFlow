6ee7b71e1f8b6dd213c411ca7ca76ca564a038a2
Bob <test69661@gmail.com>
2017-06-11 23:40:46 +0200

    Initial commit
    Initial commit

---
+++ AlphaProject/AlphaProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlphaProject
+//
+//  Created by Cociu Laurentiu-Victor on 11/06/2017.
+//  Copyright © 2017 SomeCompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
